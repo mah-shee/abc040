01c7cfa3be91c7f64d61b2722624230ee878e614
masa <masatoshi.sboard@gmail.com>
2020-11-14 23:53:56 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,8 +1,20 @@
 #[allow(unused_imports)]
 use proconio::marker::{Bytes, Chars, Usize1};
 use proconio::{fastout, input};
+use std::cmp::min;
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        a: [isize; n],
     }
+    let mut dp = vec![1 << 60; n];
+    dp[0] = 0;
+    for i in 1..n {
+        dp[i] = min(dp[i], dp[i - 1] + (a[i] - a[i - 1]).abs());
+        if i > 1 {
+            dp[i] = min(dp[i], dp[i - 2] + (a[i] - a[i - 2]).abs());
+        }
+    }
+    println!("{}", dp[n - 1]);
 }
